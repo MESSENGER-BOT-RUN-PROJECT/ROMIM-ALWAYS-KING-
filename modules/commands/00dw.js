59f4bd169cba6ad5b90d19b5660c82017f706fcf
Romim-7966 <161460526+Romim-7966@users.noreply.github.com>
2024-02-28 09:40:11 +0600

    Update 00dw.js

--- modules/commands/00dw.js
+++ modules/commands/00dw.js
@@ -1,11 +1,10 @@
 module.exports.config = {
-  name: "autodl",
+  name: "autodlvd",
   version: "1.0.",
   hasPermssion: 0,
-  credits: "Dr",
+  credits: "Shaon Ahmed",
   description: "All  Video Downloader",
   commandCategory: "Downloader",
-  usePrefix: true,
   usages: "[video link]",
   cooldowns: 0
 };
@@ -13,26 +12,30 @@ module.exports.config = {
 module.exports.handleEvent = async function ({ api, event, client, __GLOBAL }) {
   const axios = require('axios');
 const fs = require('fs-extra');
-let dipto = event.body ? event.body : '';
+let shaon = event.body ? event.body : '';
   try {
-if (dipto.startsWith('https://vt.tiktok.com') || dipto.startsWith('https://www.facebook.com') || dipto.startsWith('https://www.instagram.com/') || dipto.startsWith('https://youtu.be/') || dipto.startsWith('https://youtube.com/') || dipto.startsWith('https://x.com/') || dipto.startsWith('https://twitter.com/') || dipto.startsWith('https://vm.tiktok.com') || dipto.startsWith('https://fb.watch')){
+if (shaon.startsWith('https://vt.tiktok.com') ||
+    shaon.startsWith("https://vm.tiktok.com") ||
+    shaon.startsWith('https://www.facebook.com') || 
+    shaon.startsWith('https://fb.watch')||
+    shaon.startsWith('https://www.instagram.com/')|| shaon.startsWith('https://youtu.be/') || shaon.startsWith('https://youtube.com/')){
   api.sendMessage("downloading video, please wait...", event.threadID, event.messageID);
-  if (!dipto) {
+  if (!shaon) {
     api.sendMessage("please put a valid video link", event.threadID, event.messageID);
     return;
     }
-    const path = __dirname + `/cache/diptoo.mp4`;
-    const aa = await axios.get(`https://d1pt0-all.onrender.com/xnxx?url=${encodeURI(dipto)}`);
+    const path = __dirname + `/cache/shaonn.mp4`;
+    const aa = await axios.get(`https://d1pt0-all.onrender.com/xnxx?url=${encodeURI(shaon)}`);
    const bb = aa.data;
     const vid = (await axios.get(bb.result, { responseType: "arraybuffer", })).data;
     fs.writeFileSync(path, Buffer.from(vid, 'utf-8'));
     api.sendMessage({
       body: `${bb.cp}`,
       attachment: fs.createReadStream(path) }, event.threadID, () => fs.unlinkSync(path), event.messageID)}
-if (dipto.startsWith('https://i.imgur.com')){
-  const dipto3 = dipto.substring(dipto.lastIndexOf('.'));
-  const response = await axios.get(dipto, { responseType: 'arraybuffer' });
-const filename = __dirname + `/cache/dipto${dipto3}`;
+if (shaon.startsWith('https://i.imgur.com')){
+  const shaon3 = shaon.substring(shaon.lastIndexOf('.'));
+  const response = await axios.get(shaon, { responseType: 'arraybuffer' });
+const filename = __dirname + `/cache/shaon${shaon3}`;
     fs.writeFileSync(filename, Buffer.from(response.data, 'binary'));
     api.sendMessage({body: `Downloaded from link`,attachment: fs.createReadStream(filename)},event.threadID,
   () => fs.unlinkSync(filename),event.messageID)
