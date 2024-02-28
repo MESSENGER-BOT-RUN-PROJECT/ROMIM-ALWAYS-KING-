9f38bc1d9e5d0faf9ca749d06c21916a3d129006
Romim-7966 <161460526+Romim-7966@users.noreply.github.com>
2024-02-28 20:14:33 +0600

    Update 00im.js

--- modules/commands/00im.js
+++ modules/commands/00im.js
@@ -16,8 +16,8 @@ module.exports.run = async ({ api, event }) => {
 const axios = global.nodemodule['axios'];  
 var linkanh = event.messageReply.attachments[0].url || args.join(" ");
 	if(!linkanh) return api.sendMessage('[⚜️]→ আপনার ফটো লিংক', event.threadID, event.messageID)
-const res = await axios.get(`https://shaon-all.onrender.com/imgurv2?link=${encodeURIComponent(linkanh)}`);    
-var img = res.data.uploaded.image;
+const res = await axios.get(`https://all-image-genator-d1p.onrender.com/dipto/imgur?url=${encodeURIComponent(linkanh)}`);    
+var img = res.data.data;
     return api.sendMessage(`${img}`, event.threadID, event.messageID);
 	
 }
