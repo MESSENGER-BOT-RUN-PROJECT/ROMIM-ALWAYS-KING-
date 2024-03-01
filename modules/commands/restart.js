76ace603c5cfc83ef399e7d1895b21b917b77487
MESSENGER-BOT-RUN-PROJECT <161622093+MESSENGER-BOT-RUN-PROJECT@users.noreply.github.com>
2024-03-01 13:19:23 +0600

    Update restart.js

--- modules/commands/restart.js
+++ modules/commands/restart.js
@@ -12,5 +12,6 @@ module.exports.config = {
 
 module.exports.run = async ({ api, event, args }) => {
 	const { threadID, messageID } = event;
-	return api.sendMessage(`${global.config.BOTNAME} are now Restarting...`, threadID, () => process.exit(1));
+	return api.sendMessage(`[❶❶❶]⁖℘༒𝚁𝙾𝙼𝙸𝙼 𝙱𝙾𝚃 𝙰𝚁𝙴 𝙽𝙾𝚆 𝚁𝙴𝚂𝚃𝙰𝚁𝚃 亗[❶❶❶]\n[❶❶❶]𖤍𖤓𝚃𝙷𝙰𝙽𝙺'𝚂 𝙵𝙾𝚁 𝚄𝚂𝙸𝙽𝙶 𝚁𝙾𝙼𝙸𝙼 𝙱𝙾𝚃 —͟͟͞͞𖣘\n[❶❶❶] [❶❶❶]亗𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙷𝙸𝙼 : Facebook.com/100087320919723_______[❶❶❶]\n
+[❶❶❶]꧁♧𝙰𝙱𝙱𝚄 𝚁𝙾𝙼𝙸𝙼 ☠︎︎✪2.0[❶❶❶] `, threadID, () => process.exit(1));
 }
